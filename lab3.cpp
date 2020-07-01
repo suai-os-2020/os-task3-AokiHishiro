5ae8d2c6d64108b817677238a4520ddbaf746e7e
AokiHishiro <s4p@mail.ru>
2020-07-01 22:54:24 +0300

    add  correct 'degf'+ fix bc,lab3_init-main

--- lab3.cpp
+++ lab3.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-#define THREADCOUNT 3
+#define THREADCOUNT 7
 
 
 HANDLE MutexLock;
 HANDLE semStart;
-HANDLE threads[3];
+HANDLE threads[7];
 DWORD thread_id;
 
-HANDLE SemB, SemC;
+HANDLE;
 
 unsigned int lab3_thread_graph_id()
 {
@@ -30,7 +30,7 @@ const char* lab3_sequential_threads()
 }
 
 DWORD WINAPI thread_a(LPVOID) {
-	WaitForSingleObject(semStart, INFINITE);
+
 	for (int i = 0; i < 3; i++) {
 		WaitForSingleObject(MutexLock, INFINITE);
 		cout << 'a' << flush;
@@ -43,8 +43,6 @@ DWORD WINAPI thread_a(LPVOID) {
 
 DWORD WINAPI thread_b(LPVOID) {
 
-	WaitForSingleObject(threads[0], INFINITE);
-
 	for (int i = 0; i < 3; i++) {
 		WaitForSingleObject(MutexLock, INFINITE);
 		cout << 'b' << flush;
@@ -52,14 +50,10 @@ DWORD WINAPI thread_b(LPVOID) {
 		computation();
 	}
 
-	ReleaseSemaphore(SemB, 1, NULL);
-
 	return TRUE;
 }
 
 DWORD WINAPI thread_c(LPVOID) {
-
-	WaitForSingleObject(threads[0], INFINITE);
 
 	for (int i = 0; i < 3; i++) {
 		WaitForSingleObject(MutexLock, INFINITE);
@@ -68,25 +62,61 @@ DWORD WINAPI thread_c(LPVOID) {
 		computation();
 	}
 
-	ReleaseSemaphore(SemC, 1, NULL);
-
 	return TRUE;
 }
 
 
-DWORD WINAPI thread_k(LPVOID) {
-	WaitForSingleObject(threads[1], INFINITE);
-	WaitForSingleObject(threads[2], INFINITE);
+DWORD WINAPI thread_d(LPVOID) {
 
 	for (int i = 0; i < 3; i++) {
 		WaitForSingleObject(MutexLock, INFINITE);
-		cout << 'k' << flush;
+		cout << 'd' << flush;
 		ReleaseMutex(MutexLock);
 		computation();
 	}
 
 	return TRUE;
 }
+
+DWORD WINAPI thread_e(LPVOID) {
+
+	for (int i = 0; i < 3; i++) {
+		WaitForSingleObject(MutexLock, INFINITE);
+		cout << 'e' << flush;
+		ReleaseMutex(MutexLock);
+		computation();
+	}
+
+	return TRUE;
+}
+
+DWORD WINAPI thread_f(LPVOID) {
+
+	for (int i = 0; i < 3; i++) {
+		WaitForSingleObject(MutexLock, INFINITE);
+		cout << 'f' << flush;
+		ReleaseMutex(MutexLock);
+		computation();
+	}
+
+
+	return TRUE;
+}
+
+
+DWORD WINAPI thread_g(LPVOID) {
+
+	for (int i = 0; i < 3; i++) {
+		WaitForSingleObject(MutexLock, INFINITE);
+		cout << 'g' << flush;
+		ReleaseMutex(MutexLock);
+		computation();
+	}
+
+	return TRUE;
+}
+
+
 
 int lab3_init()
 {
@@ -102,36 +132,47 @@ int lab3_init()
 	if (semStart == NULL) {
 		cerr << "CreateSemaphore error: " << GetLastError() << '\n'; return 1;
 	}
-	SemC = CreateSemaphore(NULL, 0, 6, NULL);
-	if (SemC == NULL) {
-		cerr << "CreateSemaphore error: " << GetLastError() << '\n'; return 1;
-	}
-	SemB = CreateSemaphore(NULL, 0, 6, NULL);
-	if (SemB == NULL) {
-		cerr << "CreateSemaphore error: " << GetLastError() << '\n'; return 1;
-	}
+
+	WaitForSingleObject(semStart, INFINITE);
 
 	threads[0] = CreateThread(NULL, 0, thread_a, NULL, 0, &thread_id);
 	if (threads[0] == NULL) {
 		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
 	}
+	WaitForSingleObject(threads[0], INFINITE);
 	threads[1] = CreateThread(NULL, 0, thread_b, NULL, 0, &thread_id);
-	if (threads[0] == NULL) {
+	if (threads[1] == NULL) {
 		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
 	}
 	threads[2] = CreateThread(NULL, 0, thread_c, NULL, 0, &thread_id);
-	if (threads[0] == NULL) {
+	if (threads[2] == NULL) {
 		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
 	}
 	WaitForSingleObject(threads[1], INFINITE);
 	WaitForSingleObject(threads[2], INFINITE);
+	threads[3] = CreateThread(NULL, 0, thread_d, NULL, 0, &thread_id);
+	if (threads[3] == NULL) {
+		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
+	}
+	threads[4] = CreateThread(NULL, 0, thread_e, NULL, 0, &thread_id);
+	if (threads[4] == NULL) {
+		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
+	}
+	threads[5] = CreateThread(NULL, 0, thread_f, NULL, 0, &thread_id);
+	if (threads[5] == NULL) {
+		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
+	}
+	threads[6] = CreateThread(NULL, 0, thread_g, NULL, 0, &thread_id);
+	if (threads[6] == NULL) {
+		cerr << "CreateThread error: " << GetLastError() << '\n'; return 1;
+	}
 
+	WaitForMultipleObjects(7, threads, TRUE, INFINITE);
+	CloseHandle(semStart);
 
-	CloseHandle(SemB);
-	CloseHandle(SemC);
 	CloseHandle(MutexLock);
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 7; i++) {
 		CloseHandle(threads[i]);
 	}
 
